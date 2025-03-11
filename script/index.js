fcddfaca65f700f531862dea681ea376a8b36169
ismail-dev-code <ismail.online0385678@gmail.com>
2025-03-11 23:49:22 +0600

    dynamically grid cards added

--- script/index.js
+++ script/index.js
@@ -6,24 +6,28 @@ function loadCategories() {
     // 3. send data to display
     .then((data) => displayCategories(data.categories));
 }
-loadCategories();
 
-// 
+function loadVideos() {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((response) => response.json())
+    .then((data) => displayVideos(data.videos));
+}
+//
 //     "category_id": "1001",
 //     "category": "Music"
-// 
+//
 
 function displayCategories(categories) {
   // get the container
   const categoryContainer = document.getElementById("category-container");
   // Loop operation on array of object
   for (let cat of categories) {
-    console.log(cat);
+    // console.log(cat);
     // create element
     const categoryDiv = document.createElement("div");
     categoryDiv.innerHTML = ` 
-                  
-                <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>             
+        
+        <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>             
         
         `;
     // append the element
@@ -31,4 +35,57 @@ function displayCategories(categories) {
   }
 }
 
+const displayVideos = (videos) => {
+  // console.log(videos);
+  const videoContainer = document.getElementById("video-container");
+  videos.forEach((video) => {
+    const videoCard = document.createElement("div");
+    videoCard.innerHTML = `
+<div class="card bg-base-100">
+            <figure class="relative">
+              <img class="w-full h-[200px] object-cover "
+                src="${video.thumbnail}" />
+                <span class="absolute bottom-2 right-2 text-white text-sm bg-black rounded px-2 ">3hrs 56 min ago</span>
+            </figure>
+            <div class="py-5 flex gap-3 px-0">
+              <div class="profile">
+                <div class="avatar">
+                    <div class="ring-primary ring-offset-base-100 w-6 rounded-full ring ring-offset-2">
+                      <img src="${video.authors[0].profile_picture}" />
+                    </div>
+                  </div>
+              </div>
+              <div class="intro">
+                <h2 class="text-sm font-semibold">Midnight Serenade</h2>
+                <p class="text-sm flex gap-1 text-gray-400">${video.authors[0].profile_name} <img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=SRJUuaAShjVD&format=png" alt=""></p>
+                <p class="text-sm text-gray-400">${video.others.views}views</p>
+              </div>
+            </div>
+          </div>
 
+            `;
+    videoContainer.append(videoCard);
+  });
+};
+
+loadCategories();
+loadVideos();
+
+// {
+//     "category_id": "1001",
+//     "video_id": "aaaa",
+//     "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+//     "title": "Shape of You",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+//             "profile_name": "Olivia Mitchell",
+//             "verified": ""
+//         }
+//     ],
+//     "others": {
+//         "views": "100K",
+//         "posted_date": "16278"
+//     },
+//     "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+// }
