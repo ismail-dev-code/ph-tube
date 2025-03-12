8b19b9f66e14b60af16430cbb485624c7ea0da26
ismail-dev-code <ismail.online0385678@gmail.com>
2025-03-12 09:03:29 +0600

    video details visible while click the show details btn done

--- script/index.js
+++ script/index.js
@@ -21,10 +21,40 @@ function loadVideos() {
     .then((data) => {
       removeActiveClass();
       document.getElementById("btn-all").classList.add("active");
-      
+
       displayVideos(data.videos);
     });
 }
+
+function loadVideoDetails(videoId) {
+  const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayVideoDetails(data.video));
+}
+
+const displayVideoDetails = (video) => {
+  document.getElementById("video_details").showModal();
+  const detailsContainer = document.getElementById("details-container");
+  detailsContainer.innerHTML = `
+                            <div class="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>${video.description}</p>
+    <div class="card-actions justify-end">
+      
+    </div>
+  </div>
+</div>
+  `;
+};
+
 //
 //     "category_id": "1001",
 //     "category": "Music"
@@ -98,10 +128,19 @@ const displayVideos = (videos) => {
               </div>
               <div class="intro">
                 <h2 class="text-sm font-semibold">Midnight Serenade</h2>
-                <p class="text-sm flex gap-1 text-gray-400">${video.authors[0].profile_name} <img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=SRJUuaAShjVD&format=png" alt=""></p>
+                <p class="text-sm flex gap-1 text-gray-400">${
+                  video.authors[0].profile_name
+                } ${
+      video.authors[0].verified == true
+        ? `<img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=SRJUuaAShjVD&format=png" alt="">`
+        : ``
+    } </p>
                 <p class="text-sm text-gray-400">${video.others.views}views</p>
               </div>
             </div>
+            <button onclick="loadVideoDetails('${
+              video.video_id
+            }')" class="btn btn-block">Show Details</button>
           </div>
 
             `;
